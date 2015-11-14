96802c53a88153c1b4f0490ed0be2ee85af01cdd
MSantilal <MSantilal@users.noreply.github.com>
2015-11-14 15:22:03 +0000

    Structural changes, removal of comments

---
+++ SortOfRandomFileChooser/ApplicationInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace SortOfRandomFileChooser
+{
+    public class ApplicationInfo
+    {
+        public string AssemblyTitle { get; private set; }
+        public string AssemblyCopyright { get; private set; }
+        public string AssemblyVersion { get; private set; }
+
+        public ApplicationInfo()
+        {
+            AssemblyTitle = ((AssemblyTitleAttribute) Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(AssemblyTitleAttribute), false)).Title;
+            AssemblyCopyright = ((AssemblyCopyrightAttribute) Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(AssemblyCopyrightAttribute), false)).Copyright;
+            AssemblyVersion = ((AssemblyFileVersionAttribute) Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(AssemblyFileVersionAttribute), false)).Version;
+        }
+    }
+}
--- SortOfRandomFileChooser/Program.cs
+++ SortOfRandomFileChooser/Program.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Threading;
 using Simplicity.Core;
 
 namespace SortOfRandomFileChooser
@@ -14,26 +12,17 @@ namespace SortOfRandomFileChooser
 
         public static void Main(string[] args)
         {
+            var applicationInfo = new ApplicationInfo();
+            Console.WriteLine("***************************************************************");
+            Console.WriteLine(string.Format("-- {0} {1}", applicationInfo.AssemblyTitle, applicationInfo.AssemblyVersion));
+            Console.WriteLine(string.Format("-- {0}", applicationInfo.AssemblyCopyright));
+            Console.WriteLine("***************************************************************");
+            
             GetRandomFiles();
         }
 
         private static void GetRandomFiles()
         {
-
-            // Header information about developer (Monil)
-            var assemblyTitle = ((AssemblyTitleAttribute)Attribute.GetCustomAttribute(
-                Assembly.GetExecutingAssembly(), typeof(AssemblyTitleAttribute), false)).Title;
-            var copyright = ((AssemblyCopyrightAttribute)Attribute.GetCustomAttribute(
-                Assembly.GetExecutingAssembly(), typeof(AssemblyCopyrightAttribute), false)).Copyright;
-            var version = ((AssemblyFileVersionAttribute)Attribute.GetCustomAttribute(
-                Assembly.GetExecutingAssembly(), typeof(AssemblyFileVersionAttribute), false)).Version;
-
-            Console.WriteLine("***************************************************************");
-            Console.WriteLine(string.Format("-- {0} {1}", assemblyTitle, version));
-            Console.WriteLine(string.Format("-- {0}", copyright));
-            Console.WriteLine("***************************************************************");
-
-            // User inputs for dir location, number of files and type of files to be gathered
             Console.WriteLine(String.Empty);
             Console.Write("Enter Directory Path: ");
             var directoryPath = Console.ReadLine();
@@ -42,7 +31,6 @@ namespace SortOfRandomFileChooser
             Console.Write("File Extension Required: ");
             var fileExtension = Console.ReadLine();
 
-            // Formatting dir entered for use
             if (directoryPath != null)
             {
                 if (directoryPath.Contains('"'))
@@ -51,95 +39,78 @@ namespace SortOfRandomFileChooser
                     directoryPath = directoryPath.TrimEnd('"');
                 }
 
-                // Produces a list of all files in the specified dir, and with the specified extension as given by the user above
-                // (if the user entered .java then all Java files in the dir etc.)
                 var filesInDirectory = Directory.EnumerateFiles(directoryPath, string.Format("*{0}", fileExtension), SearchOption.AllDirectories);
-                var files = new List<string>(filesInDirectory);
+                var filesInDirectoryList = new List<string>(filesInDirectory);
 
-                // Random variable created based on the current date/time
-                var random = new Random((int)DateTime.Now.Ticks);
+                var random = new Random((int) DateTime.Now.Ticks << 0xFFFFFF);
 
-                // Checks that the user entered a number of files to be randomly chosen
                 if (fileCountRequired != null)
                 {
-                    // New list created to hold randomly selected file names, for loop cycles 
                     listOfFilesFound = new List<string>();
 
-                    // Variable for stopping condition on for loop below
-                    int j = int.Parse(fileCountRequired);
+                    var fileCount = int.Parse(fileCountRequired);
 
-                    for (int i = 0; i < j; i++)
+                    for (var i = 0; i < fileCount; i++)
                     {
-                        var fileName = files[random.Next(0, files.Count)];
-                        bool fileExists;
+                        var fileName = filesInDirectoryList[random.Next(0, filesInDirectoryList.Count)];
 
-                        if (fileExists = listOfFilesFound.Exists(element => element == fileName))
+                        if (!listOfFilesFound.Exists(e => e == fileName))
                         {
-                            Console.WriteLine("*******************");
-                            Console.WriteLine("*******************");
-                            Console.WriteLine("Duplicate file found, skipping file and adding 1 to number of files required");
-                            Console.WriteLine("*******************");
-                            Console.WriteLine("*******************");
-                            j += 1;
-
+                            listOfFilesFound.Add(fileName);
                         }
                         else
                         {
-                            listOfFilesFound.Add(fileName);
-                            Console.WriteLine(fileName);
-
+                            fileCount++;
                         }
                     }
 
-                    Console.WriteLine("Files Found. Copy to local directory? Y/N");
-                    CopyFilesToFolder();
+                    Console.WriteLine("The following files were selected: ");
+                    foreach (var fileFound in listOfFilesFound)
+                    {
+                        Console.WriteLine(fileFound);
+                    }
+
+                    Console.WriteLine("Copy to Local Directory? Y/N");
+                    Console.Write("Response: ");
+                    var writeToFileResponse = Console.ReadLine();
+
+                    if (writeToFileResponse == "Y" || writeToFileResponse == "y")
+                    {
+                        CopyFilesToFolder();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You have selected No. Many Puppies are now crying at your betrayal.");
+                        Console.WriteLine("Press any key to exit...");
+                        Console.ReadKey();
+                    }
                 }
             }
         }
 
         private static void CopyFilesToFolder()
         {
-            Console.Write("Response: ");
-            var writeToFileResponse = Console.ReadLine();
-            var duplicatedFiles = new List<string>();
-            if (writeToFileResponse == "Y" || writeToFileResponse == "y")
+            Console.WriteLine("Copying Files...");
+            File.WriteAllLines(FileManagement.RelativeToApplication("CopiedFiles.txt"), listOfFilesFound);
+            foreach (var filePaths in listOfFilesFound)
             {
-                Console.WriteLine("Copying Files...");
-                File.WriteAllLines(FileManagement.RelativeToApplication("CopiedFiles.txt"), listOfFilesFound);
-                foreach (var filePaths in listOfFilesFound)
+                var x = filePaths.Split('\\');
+                if (!Directory.Exists(FileManagement.RelativeToApplication("Random Files")))
                 {
-                    var x = filePaths.Split('\\');
-                    if (!Directory.Exists(FileManagement.RelativeToApplication("Random Files")))
-                    {
-                        Directory.CreateDirectory(FileManagement.RelativeToApplication("Random Files"));
-                    }
-
-                    if (!File.Exists(FileManagement.RelativeToApplication("Random Files", x[x.Length - 1])))
-                    {
-                        File.Copy(filePaths, FileManagement.RelativeToApplication("Random Files", x[x.Length - 1]));
-                    }
-                    else
-                    {
-                        duplicatedFiles.Add(filePaths);
-                    }
+                    Directory.CreateDirectory(FileManagement.RelativeToApplication("Random Files"));
                 }
 
-                Console.WriteLine("File Paths Exported to: {0}", FileManagement.RelativeToApplication("RetrievedFiles.txt"));
-                Console.WriteLine("Files Copied to: {0}", FileManagement.RelativeToApplication("Random Files"));
-                Console.WriteLine("The following files were not copied as they were duplicate: ");
-                foreach (var dupFiles in duplicatedFiles)
+                if (!File.Exists(FileManagement.RelativeToApplication("Random Files", x[x.Length - 1])))
                 {
-                    Console.WriteLine(dupFiles);
+                    File.Copy(filePaths, FileManagement.RelativeToApplication("Random Files", x[x.Length - 1]));
                 }
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadKey();
             }
-            else
-            {
-                Console.WriteLine("You have selected No. Many Puppies are now crying at your betrayal.");
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadKey();
-            }
+
+            Console.WriteLine("File Paths Exported to: {0}", FileManagement.RelativeToApplication("RetrievedFiles.txt"));
+            Console.WriteLine("Files Copied to: {0}", FileManagement.RelativeToApplication("Random Files"));
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
         }
     }
 }
